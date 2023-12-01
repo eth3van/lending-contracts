3123577ab3d7b49730314ca535045398afcd9764
eth3van <evanguo.eth@gmail.com>
2023-12-01 18:09:32 -0500

    Refactor BorrowingEngine contract to enhance security and modularity. Replaced Inheritance with ReentrancyGuard for improved protection against reentrancy attacks. Integrated CoreStorage for better state management, encapsulating collateral handling and validation logic. Updated borrowFunds and paybackBorrowedAmount functions to utilize CoreStorage methods for checks and state updates. Adjusted event emissions to align with the new structure, ensuring clearer tracking of borrowing and repayment actions.

--- src/BorrowingEngine.sol
+++ src/BorrowingEngine.sol
@@ -1,32 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {Inheritance} from "./Inheritance.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { CoreStorage } from "./CoreStorage.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import { HealthFactor } from "src/HealthFactor.sol";
 
-contract BorrowingEngine is Inheritance {
+contract BorrowingEngine is ReentrancyGuard {
+    CoreStorage private immutable i_coreStorage;
+
     ///////////////////////////////
     //         Functions         //
     ///////////////////////////////
 
-    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses)
-        Inheritance(tokenAddresses, priceFeedAddresses)
-    {}
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address coreStorageAddress) {
+        i_coreStorage = CoreStorage(coreStorageAddress);
+    }
 
-    function borrowFunds(address tokenToBorrow, uint256 amountToBorrow)
-        public
-        moreThanZero(amountToBorrow)
-        nonReentrant
-        isAllowedToken(tokenToBorrow)
-    {
+    function borrowFunds(address tokenToBorrow, uint256 amountToBorrow) public nonReentrant {
         _borrowFunds(tokenToBorrow, amountToBorrow);
     }
 
-    function paybackBorrowedAmount(address tokenToPayBack, uint256 amountToPayBack, address onBehalfOf)
+    function paybackBorrowedAmount(
+        address tokenToPayBack,
+        uint256 amountToPayBack,
+        address onBehalfOf
+    )
         public
-        moreThanZero(amountToPayBack)
         nonReentrant
-        isAllowedToken(tokenToPayBack)
     {
         _paybackBorrowedAmount(tokenToPayBack, amountToPayBack, onBehalfOf);
     }
@@ -35,70 +36,62 @@ contract BorrowingEngine is Inheritance {
     //         Private Functions         //
     //////////////////////////////////////
 
-    function _borrowFunds(address tokenToBorrow, uint256 amountToBorrow)
-        private
-        moreThanZero(amountToBorrow)
-        nonReentrant
-        isAllowedToken(tokenToBorrow)
-    {
+    function _borrowFunds(address tokenToBorrow, uint256 amountToBorrow) private nonReentrant {
+        i_coreStorage.moreThanZero(amountToBorrow);
+        i_coreStorage.isAllowedToken(tokenToBorrow);
         // get the amount available to borrow
-        uint256 availableToBorrow = getAvailableToBorrow(tokenToBorrow);
+        uint256 availableToBorrow = i_coreStorage.getAvailableToBorrow(tokenToBorrow);
         // if user is trying to borrow more than available, revert
         if (amountToBorrow > availableToBorrow) {
-            revert LendingEngine__NotEnoughAvailableTokens();
+            revert CoreStorage.LendingEngine__NotEnoughAvailableTokens();
         }
 
         // Get USD value of borrow amount
-        uint256 borrowAmountInUsd = _getUsdValue(tokenToBorrow, amountToBorrow);
+        uint256 borrowAmountInUsd = i_coreStorage.getUsdValue(tokenToBorrow, amountToBorrow);
 
-        // Track the specific token amount borrowed
-        getTokenBorrowed()[msg.sender][tokenToBorrow] += amountToBorrow;
-        // Track total USD borrowed
-        s_AmountBorrowed[msg.sender] += borrowAmountInUsd;
+        // Track the specific token amounts & USD amounts borrowed
+        i_coreStorage._increaseAmountOfTokenBorrowed(msg.sender, tokenToBorrow, amountToBorrow);
 
         // This will check if total borrowed exceeds collateral limits
-        revertIfHealthFactorIsBroken(msg.sender);
+        i_coreStorage.revertIfHealthFactorIsBroken(msg.sender);
 
         // attempt to send borrowed amount to the msg.sender
         bool success = IERC20(tokenToBorrow).transfer(msg.sender, amountToBorrow);
         if (!success) {
-            revert BorrowingEngine__TransferFailed();
+            revert i_coreStorage.BorrowingEngine__TransferFailed();
         }
         // emit event when msg.sender borrows funds
-        emit UserBorrowed(msg.sender, tokenToBorrow, amountToBorrow);
+        emit i_coreStorage.UserBorrowed(msg.sender, tokenToBorrow, amountToBorrow);
     }
 
-    function _paybackBorrowedAmount(address tokenToPayBack, uint256 amountToPayBack, address onBehalfOf)
+    function _paybackBorrowedAmount(
+        address tokenToPayBack,
+        uint256 amountToPayBack,
+        address onBehalfOf
+    )
         private
-        moreThanZero(amountToPayBack)
         nonReentrant
-        isAllowedToken(tokenToPayBack)
     {
+        i_coreStorage.moreThanZero(amountToPayBack);
+        i_coreStorage.isAllowedToken(tokenToPayBack);
         // if the address being paid on behalf of is the 0 address, revert
         if (onBehalfOf == address(0)) {
-            revert BorrowingEngine__ZeroAddressNotAllowed();
+            revert CoreStorage.BorrowingEngine__ZeroAddressNotAllowed();
         }
 
-        // Decrease the user's borrowed balance in our internal accounting
-        // This must happen first to prevent reentrancy attacks
-        s_AmountBorrowed[onBehalfOf] -= amountToPayBack;
-        // the line above is the same as `s_AmountBorrowed[onBehalfOf] = s_AmountBorrowed[onBehalfOf] - amountToPayBack;`
-
-        // Check if user has enough borrowed amount of this token
-        uint256 borrowedAmount = getTokenBorrowed()[onBehalfOf][tokenToPayBack];
+        // Safety check to prevent users from overpaying their debt
+        uint256 borrowedAmount = i_coreStorage.getAmountOfTokenBorrowed(onBehalfOf, tokenToPayBack);
         if (borrowedAmount < amountToPayBack) {
-            revert BorrowingEngine__NotEnoughBorrowedAmount();
+            revert CoreStorage.BorrowingEngine__OverpaidDebt();
         }
-
-        // Get USD value of payback amount
-        uint256 paybackAmountInUsd = _getUsdValue(tokenToPayBack, amountToPayBack);
 
         // Update state BEFORE external calls (CEI pattern)
-        getTokenBorrowed()[onBehalfOf][tokenToPayBack] -= amountToPayBack;
-        s_AmountBorrowed[onBehalfOf] -= paybackAmountInUsd;
+        // Decrease the user's borrowed balance in our internal accounting
+        // This must happen first to prevent reentrancy attacks
+        i_coreStorage._decreaseAmountOfTokenBorrowed(onBehalfOf, tokenToPayBack, amountToPayBack);
 
         // Check health factor after repayment
-        revertIfHealthFactorIsBroken(onBehalfOf);
+        i_coreStorage.revertIfHealthFactorIsBroken(onBehalfOf);
 
         // Transfer tokens from user to contract
         bool success = IERC20(tokenToPayBack).transferFrom(msg.sender, address(this), amountToPayBack);
@@ -106,9 +99,9 @@ contract BorrowingEngine is Inheritance {
         // Check if transfer was successful
         // This is a backup check since transferFrom would normally revert on failure
         if (!success) {
-            revert BorrowingEngine__TransferFailed();
+            revert CoreStorage.BorrowingEngine__TransferFailed();
         }
         // emit event
-        emit BorrowedAmountRepaid(msg.sender, onBehalfOf, tokenToPayBack, amountToPayBack);
+        emit CoreStorage.BorrowedAmountRepaid(msg.sender, onBehalfOf, tokenToPayBack, amountToPayBack);
     }
 }
