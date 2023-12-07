3cd40d4d78f748d9d4711418aef214d26911f0eb
eth3van <evanguo.eth@gmail.com>
2023-12-06 22:54:32 -0500

    Refactor LendingCore contract to inherit from InterestRate
    - Updated the LendingCore contract to inherit from the InterestRate contract instead of Liquidations.
    - Adjusted the constructor to initialize the InterestRate with token and price feed addresses.
    - Added placeholders for shared functions related to deposit, borrow, payback, and withdraw, indicating future implementation plans.

--- src/LendingCore.sol
+++ src/LendingCore.sol
@@ -1,13 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { Liquidations } from "src/Liquidations.sol";
+import { InterestRate } from "src/InterestRate.sol";
 
-contract LendingCore is Liquidations {
+contract LendingCore is InterestRate {
     constructor(
         address[] memory tokenAddresses,
         address[] memory priceFeedAddresses
     )
-        Liquidations(tokenAddresses, priceFeedAddresses)
+        InterestRate(tokenAddresses, priceFeedAddresses)
     { }
+
+    // Shared Functions
+
+    // deposit and borrow
+
+    // payback and withdraw (should probaly be in borrow.sol)
+
+    //
 }
