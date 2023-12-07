1fd22f0e7d6bba18a8d3eff7bdc5aebc8a27c08d
eth3van <evanguo.eth@gmail.com>
2023-12-06 22:54:42 -0500

    Implement withdrawCollateral functionality in Withdraw contract
    - Added the withdrawCollateral function to allow users to withdraw their collateral, ensuring they have sufficient balance and have paid back any borrowed amounts.
    - Introduced an internal _withdrawCollateral function to handle the withdrawal process, including balance checks and transferring tokens.
    - Integrated error handling using the Errors library for consistent messaging on withdrawal conditions.
    - Updated comments to clarify the purpose and parameters of the withdraw function.

--- src/Withdraw.sol
+++ src/Withdraw.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.20;
 
 import { Borrowing } from "src/Borrowing.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { Errors } from "src/libraries/Errors.sol";
 
 contract Withdraw is Borrowing {
     constructor(
@@ -11,11 +13,12 @@ contract Withdraw is Borrowing {
         Borrowing(tokenAddresses, priceFeedAddresses)
     { }
     /*
-     * @param tokenCollateralAddress: The ERC20 token address of the collateral you're redeeming
-     * @param amountCollateral: The amount of collateral you're redeeming
+     * @param tokenCollateralAddress: The ERC20 token address of the collateral you're withdrawing
+     * @param amountCollateral: The amount of collateral you're withdrawing
      * @notice This function will withdraw your collateral.
      * @notice If you have borrowed funds, you will not be able to redeem until you pay back borrowed funds
      */
+
     function withdrawCollateral(
         address tokenCollateralAddress,
         uint256 amountCollateral
@@ -25,10 +28,42 @@ contract Withdraw is Borrowing {
         nonReentrant
         isAllowedToken(tokenCollateralAddress)
     {
+        _withdrawCollateral(tokenCollateralAddress, amountCollateral, msg.sender, msg.sender);
+        _revertIfHealthFactorIsBroken(msg.sender);
+    }
+
+    function _withdrawCollateral(
+        address tokenCollateralAddress,
+        uint256 amountCollateralToWithdraw,
+        address from,
+        address to
+    )
+        internal
+    {
         // user must have funds deposited to withdraw
+        if (_getCollateralBalanceOfUser(from, tokenCollateralAddress) == 0) {
+            revert Errors.Withdraw__UserHasCollateralDeposited();
+        }
         // user can only withdraw up to the amount he deposited
+        if (_getCollateralBalanceOfUser(from, tokenCollateralAddress) < amountCollateralToWithdraw) {
+            revert Errors.Withdraw__UserDoesNotHaveThatManyTokens();
+        }
         // user must pay back the amount he borrowed before withdraw
-        // withdraw
-        // emit event
+
+        // Decrease the user's collateral balance in our internal accounting
+        // This must happen before the transfer to prevent reentrancy attacks
+        decreaseUserDebtAndTotalDebtBorrowed(from, tokenCollateralAddress, amountCollateralToWithdraw);
+
+        // Emit event for off-chain tracking and transparency since we are updating state
+        emit CollateralWithdrawn(from, to, tokenCollateralAddress, amountCollateralToWithdraw);
+
+        // Transfer the collateral tokens from this contract back to the user
+        // Using ERC20's transfer instead of transferFrom since the tokens are already in this contract
+        bool success = IERC20(tokenCollateralAddress).transfer(to, amountCollateralToWithdraw);
+
+        // If the transfer fails, revert the transaction
+        if (!success) {
+            revert Errors.TransferFailed();
+        }
     }
 }
