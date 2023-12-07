05ebf8ba408e1488269a7a5bbd647dfb6f3cc06f
eth3van <evanguo.eth@gmail.com>
2023-12-06 22:54:26 -0500

    Add liquidation functionality to Liquidations contract
    - Introduced the liquidate function to initiate liquidations for users with insufficient health factors.
    - Implemented the private _liquidate function to handle the liquidation process, including collateral seizure and debt repayment.
    - Added error handling to ensure liquidations only occur when the user's health factor is below the minimum threshold and improved health factor post-liquidation.
    - Integrated the Errors library for consistent error messaging during liquidation operations.

--- src/Liquidations.sol
+++ src/Liquidations.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 import { Withdraw } from "src/Withdraw.sol";
+import { Errors } from "src/libraries/Errors.sol";
 
 contract Liquidations is Withdraw {
     constructor(
@@ -10,4 +11,47 @@ contract Liquidations is Withdraw {
     )
         Withdraw(tokenAddresses, priceFeedAddresses)
     { }
+
+    function liquidate(address collateral, address user, uint256 debtAmoutToPay) external nonReentrant { }
+
+    function _liquidate(
+        address collateral,
+        address user,
+        uint256 debtAmoutToPay
+    )
+        private
+        isAllowedToken(collateral)
+        moreThanZero(debtAmoutToPay)
+    {
+        // Get the user's initial health factor to:
+        // 1. Verify they can be liquidated (health factor < MIN_HEALTH_FACTOR)
+        // 2. Compare with their final health factor after liquidation
+        uint256 startingUserHealthFactor = _healthFactor(user);
+        if (startingUserHealthFactor >= _getMinimumHealthFactor()) {
+            revert Errors.Liquidations__HealthFactorOk();
+        }
+
+        // Calculate how much collateral to seize based on the debt amount
+        // Example: If paying $100 of debt and ETH price is $2000, then tokenAmountFromDebtCovered = 0.05 ETH
+        uint256 tokenAmountFromDebtPaid = _getTokenAmountFromUsd(collateral, debtAmoutToPay);
+
+        // Calculate the bonus collateral for the liquidator (incentive for performing liquidation)
+        // Example: If LIQUIDATION_BONUS is 10%, then bonusCollateral = 0.005 ETH
+        uint256 bonusCollateral = (tokenAmountFromDebtPaid * _getLiquidationBonus()) / _getLiquidationPrecision();
+
+        // Payback the debt from the user's account
+        paybackBorrowedAmount(collateral, debtAmoutToPay, user);
+
+        // Seize the collateral from the user plus the bonus and send it to the liquidator
+        _withdrawCollateral(collateral, tokenAmountFromDebtPaid + bonusCollateral, user, msg.sender);
+
+        // Verify that the liquidation actually helped (improved the user's health factor)
+        uint256 endingUserHealthFactor = _healthFactor(user);
+        if (endingUserHealthFactor <= startingUserHealthFactor) {
+            revert Errors.Liquidations__HealthFactorNotImproved();
+        }
+
+        // Make sure the liquidator's health factor is still good after liquidation
+        _revertIfHealthFactorIsBroken(msg.sender);
+    }
 }
