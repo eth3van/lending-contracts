6e3e102825ea53287423ae7bccb78091d0414d7a
eth3van <evanguo.eth@gmail.com>
2023-12-07 09:49:53 -0500

    Enhance Errors library with new error types for borrowing and liquidation
    - Added new error types: Borrowing__NotEnoughTokensToPayDebt(), Liquidations__UserHasNoCollateralDeposited(), Liquidations__InsufficientBalanceToLiquidate(), Liquidations__DebtAmountExceedsBorrowedAmount(), and Liquidations__CantLiquidateSelf().
    - Removed the Borrowing__ZeroAddressNotAllowed() error for improved consistency in error handling.
    - Updated existing error types to enhance clarity and maintainability across borrowing and liquidation processes.

--- src/libraries/Errors.sol
+++ src/libraries/Errors.sol
@@ -11,8 +11,8 @@ library Errors {
     //     Borrowing Errors     //
     ///////////////////////////////
     error Borrowing__OverpaidDebt();
-    error Borrowing__ZeroAddressNotAllowed();
     error Borrowing__NotEnoughAvailableCollateral();
+    error Borrowing__NotEnoughTokensToPayDebt();
 
     ///////////////////////////////
     //    CoreStorage Errors    //
@@ -30,12 +30,17 @@ library Errors {
     error AmountNeedsMoreThanZero();
     error TokenNotAllowed(address token);
     error TransferFailed();
+    error ZeroAddressNotAllowed();
 
     ///////////////////////////////
     //    Liquidation Errors    //
     //////////////////////////////
     error Liquidations__HealthFactorOk();
     error Liquidations__HealthFactorNotImproved();
+    error Liquidations__UserHasNoCollateralDeposited();
+    error Liquidations__InsufficientBalanceToLiquidate();
+    error Liquidations__DebtAmountExceedsBorrowedAmount();
+    error Liquidations__CantLiquidateSelf();
 
     //////////////////////////////
     //     Withdraw Errors     //
