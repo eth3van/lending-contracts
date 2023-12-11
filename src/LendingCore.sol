adc3a52f41000428c5f875cd4f86f7b426821771
eth3van <evanguo.eth@gmail.com>
2023-12-11 04:33:49 -0500

    Refactor LendingCore contract to integrate LiquidationEngine and Withdraw functionality
    - Replaced inheritance from InterestRate with Withdraw to streamline the contract structure.
    - Introduced LiquidationEngine for handling liquidation operations, enhancing the contract's capabilities.
    - Updated the constructor to initialize LiquidationEngine and adjusted the liquidate function to delegate calls to it.
    - Improved code organization and maintainability by clarifying the purpose of functions related to liquidation.

--- src/LendingCore.sol
+++ src/LendingCore.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { InterestRate } from "src/InterestRate.sol";
+import { Withdraw } from "./Withdraw.sol";
+import { LiquidationEngine } from "./Liquidation/LiquidationEngine.sol";
 
-contract LendingCore is InterestRate {
+contract LendingCore is Withdraw {
+    LiquidationEngine public liquidationEngine;
+
     constructor(
         address[] memory tokenAddresses,
         address[] memory priceFeedAddresses,
@@ -11,12 +14,17 @@ contract LendingCore is InterestRate {
         address automationRegistry,
         uint256 upkeepId
     )
-        InterestRate(tokenAddresses, priceFeedAddresses, swapRouter, automationRegistry, upkeepId)
-    { }
+        Withdraw(tokenAddresses, priceFeedAddresses)
+    {
+        liquidationEngine = new LiquidationEngine(address(this), swapRouter, automationRegistry, upkeepId);
+    }
 
-    // Shared Functions
-
-    // deposit and borrow
+    // Delegate liquidation calls to LiquidationEngine
+    function liquidate(address user, address collateral, address debtToken, uint256 debtAmountToPay) external {
+        liquidationEngine.liquidate(user, collateral, debtToken, debtAmountToPay);
+    }
 
     // payback and withdraw
+
+    // deposit and borrow
 }
