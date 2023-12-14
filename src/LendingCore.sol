c100e07415e2217145b8013ab74c83d4a1f01a1b
eth3van <evanguo.eth@gmail.com>
2023-12-14 01:13:09 -0500

    Implement liquidation functions and access control in LendingCore contract
    - Added OnlyLiquidationEngine modifier to restrict access to specific functions, enhancing security.
    - Introduced liquidationWithdrawCollateral function for controlled collateral withdrawal by the LiquidationEngine.
    - Implemented liquidationDecreaseDebt function to manage debt reduction and payment collection, ensuring proper handling of user debts.
    - Updated liquidate function to include the caller's address, improving the liquidation process flow.

--- src/LendingCore.sol
+++ src/LendingCore.sol
@@ -3,9 +3,19 @@ pragma solidity ^0.8.20;
 
 import { Withdraw } from "./Withdraw.sol";
 import { LiquidationEngine } from "./Liquidation/LiquidationEngine.sol";
+import { Errors } from "./libraries/Errors.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract LendingCore is Withdraw {
     LiquidationEngine public liquidationEngine;
+
+    modifier OnlyLiquidationEngine() {
+        // Only LiquidationEngine can call this
+        if (msg.sender != address(liquidationEngine)) {
+            revert Errors.LendingCore__OnlyLiquidationEngine();
+        }
+        _;
+    }
 
     constructor(
         address[] memory tokenAddresses,
@@ -21,10 +31,41 @@ contract LendingCore is Withdraw {
 
     // Delegate liquidation calls to LiquidationEngine
     function liquidate(address user, address collateral, address debtToken, uint256 debtAmountToPay) external {
-        liquidationEngine.liquidate(user, collateral, debtToken, debtAmountToPay);
+        liquidationEngine.liquidate(msg.sender, user, collateral, debtToken, debtAmountToPay);
     }
 
     // payback and withdraw
 
     // deposit and borrow
+
+    function liquidationWithdrawCollateral(
+        address collateral,
+        uint256 amount,
+        address user,
+        address recipient
+    )
+        external
+        OnlyLiquidationEngine
+    {
+        _withdrawCollateral(collateral, amount, user, recipient);
+    }
+
+    function liquidationDecreaseDebt(
+        address token,
+        uint256 amount,
+        address user,
+        address payer
+    )
+        external
+        OnlyLiquidationEngine
+    {
+        // First decrease the debt (state change)
+        decreaseUserDebtAndTotalDebtBorrowed(user, token, amount);
+
+        // Then collect the payment
+        bool success = IERC20(token).transferFrom(payer, address(this), amount);
+        if (!success) {
+            revert Errors.TransferFailed();
+        }
+    }
 }
