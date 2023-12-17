a76ada173c505a776809ac99d784c124f5d8b21a
eth3van <evanguo.eth@gmail.com>
2023-12-17 04:59:08 -0500

    Add setAutomationContract function to LendingCore contract for owner-controlled automation setup

--- src/LendingCore.sol
+++ src/LendingCore.sol
@@ -95,4 +95,8 @@ contract LendingCore is Withdraw, Ownable {
             revert Errors.TransferFailed();
         }
     }
+
+    function setAutomationContract(address automationContract) external onlyOwner {
+        liquidationEngine.setAutomationContract(automationContract);
+    }
 }
