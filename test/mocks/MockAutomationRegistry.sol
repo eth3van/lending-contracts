a4d2131f28a87825edccebb497df41ee31560799
eth3van <evanguo.eth@gmail.com>
2023-12-17 05:00:33 -0500

    Add MockAutomationRegistry contract for testing automation functionality
    - Introduced MockAutomationRegistry.sol to simulate automation registry behavior in tests.
    - Implemented addFunds function to facilitate LINK token transfers for mock scenarios.
    - Enhanced testing capabilities by providing a mock implementation for automation interactions.

---
+++ test/mocks/MockAutomationRegistry.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { IAutomationRegistryInterface } from "src/interfaces/IAutomationRegistryInterface.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract MockAutomationRegistry is IAutomationRegistryInterface {
+    address public immutable LINK;
+
+    constructor(address linkToken) {
+        LINK = linkToken;
+    }
+
+    function addFunds(uint256, /* id */ uint96 amount) external {
+        // Mock implementation - just transfer LINK tokens from sender to this contract
+        IERC20(LINK).transferFrom(msg.sender, address(this), amount);
+    }
+}
