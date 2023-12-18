ca69c77029c702e79c31a8ad1df824d27e93216e
eth3van <evanguo.eth@gmail.com>
2023-12-18 09:19:20 -0500

    clean up

--- src/Borrowing.sol
+++ src/Borrowing.sol
@@ -1,11 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { Lending } from "src/Lending.sol";
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { Errors } from "src/libraries/Errors.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { Lending } from "src/Lending.sol";
 
+/**
+ * @title Borrowing Contract
+ * @author Evan Guo
+ * @notice Manages the borrowing functionality of the lending protocol
+ * @dev Implements core borrowing logic with the following features:
+ *
+ * Core Features:
+ * 1. Token Borrowing
+ *    - Collateral-backed loans
+ *    - Multi-token support
+ *    - Real-time liquidity tracking
+ *    - Dynamic borrowing limits
+ *
+ * 2. Debt Management
+ *    - Flexible repayment system
+ *    - Altruistic debt repayment
+ *    - Overpayment protection
+ *    - Accurate debt tracking
+ *
+ * 3. Risk Management
+ *    - Health factor monitoring
+ *    - Collateral validation
+ *    - Solvency protection
+ *    - Protocol-wide accounting
+ *
+ * Security Features:
+ * - Reentrancy protection
+ * - CEI (Checks-Effects-Interactions) pattern
+ * - Input validation
+ * - Access control
+ *
+ * Inherits:
+ * - Lending: Base lending functionality and collateral management
+ */
 contract Borrowing is Lending {
+    /*//////////////////////////////////////////////////////////////
+                              CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
     constructor(
         address[] memory tokenAddresses,
         address[] memory priceFeedAddresses
@@ -13,61 +50,112 @@ contract Borrowing is Lending {
         Lending(tokenAddresses, priceFeedAddresses)
     { }
 
-    function borrowFunds(address tokenToBorrow, uint256 amountToBorrow) public nonReentrant {
-        _borrowFunds(tokenToBorrow, amountToBorrow);
-    }
+    /*//////////////////////////////////////////////////////////////
+                           INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
-    function paybackBorrowedAmount(
-        address tokenToPayBack,
-        uint256 amountToPayBack,
-        address onBehalfOf
-    )
-        public
-        nonReentrant
-    {
-        _paybackBorrowedAmount(tokenToPayBack, amountToPayBack, onBehalfOf);
-    }
-
+    /**
+     * @notice Allows users to borrow tokens against their deposited collateral
+     * @dev Implements a multi-stage validation and borrowing process with the following features:
+     *
+     * Security Features:
+     * 1. Physical token verification
+     * 2. Protocol accounting validation
+     * 3. Health factor checks
+     * 4. CEI (Checks-Effects-Interactions) pattern
+     *
+     * Process Flow:
+     * 1. Validate token availability
+     * 2. Update protocol accounting
+     * 3. Verify position health
+     * 4. Execute token transfer
+     *
+     * @param tokenToBorrow The ERC20 token address the user wants to borrow
+     * @param amountToBorrow The amount of tokens to borrow (in token's native decimals)
+     *
+     * Security Features:
+     * - Validates token allowlist to prevent unsupported assets
+     * - Ensures protocol solvency through health factor checks
+     * - Uses internal accounting before external calls (CEI pattern)
+     */
     function _borrowFunds(
         address tokenToBorrow,
         uint256 amountToBorrow
     )
-        private
+        internal
         moreThanZero(amountToBorrow)
         isAllowedToken(tokenToBorrow)
     {
-        // 1. Physical Token Check
-        // Verify the contract actually has enough tokens in its possession
-        // This prevents attempts to transfer tokens that don't exist in the contract
+        // STAGE 1: Physical Token Validation
+        // Verify the contract has sufficient tokens in its possession
+        // This prevents attempts to borrow tokens that don't physically exist in the contract
+        // Critical for maintaining protocol solvency and preventing overextension
         if (IERC20(tokenToBorrow).balanceOf(address(this)) < amountToBorrow) {
             revert Errors.Borrowing__NotEnoughAvailableCollateral();
         }
 
-        // 2. Protocol Accounting Check
-        // Get how many tokens are available according to protocol's accounting
-        // This considers all existing loans and ensures we're not lending tokens that are already promised to other borrowers
+        // STAGE 2: Protocol Accounting Validation
+        // Calculate available tokens according to protocol's internal accounting
+        // This considers all existing loans and ensures we're not lending tokens
+        // that are already promised to other borrowers
         uint256 availableToBorrow = _getAvailableToBorrow(tokenToBorrow);
-        // if user is trying to borrow more than available, revert
+
+        // Validate borrowing amount against available liquidity
+        // Prevents borrowing more than the protocol can safely lend
         if (amountToBorrow > availableToBorrow) {
             revert Errors.Borrowing__NotEnoughAvailableCollateral();
         }
 
-        // Track the specific token amounts & USD amounts borrowed
+        // STAGE 3: State Updates (Following CEI Pattern)
+        // Update internal accounting before any external interactions
+        // Track both token-specific amounts and USD value of borrowed assets
         increaseUserDebtAndTotalDebtBorrowed(msg.sender, tokenToBorrow, amountToBorrow);
 
-        // emit event when msg.sender borrows funds
+        // Emit event for off-chain tracking and transparency
+        // Critical for DApp interfaces and protocol monitoring
         emit UserBorrowed(msg.sender, tokenToBorrow, amountToBorrow);
 
-        // This will check if total borrowed exceeds collateral limits
+        // STAGE 4: Health Factor Validation
+        // Verify that this borrow doesn't break the user's health factor
+        // This ensures the position remains safely collateralized
         _revertIfHealthFactorIsBroken(msg.sender);
 
-        // attempt to send borrowed amount to the msg.sender
+        // STAGE 5: Token Transfer (Final Interaction)
+        // Execute the actual token transfer to the borrower
+        // This is the last step in accordance with CEI pattern
         bool success = IERC20(tokenToBorrow).transfer(msg.sender, amountToBorrow);
         if (!success) {
             revert Errors.TransferFailed();
         }
     }
 
+    /**
+     * @notice Allows users to repay borrowed tokens, either for themselves or on behalf of others
+     * @dev Implements a comprehensive debt repayment process with the following features:
+     *
+     * Security Features:
+     * 1. Input validation
+     * 2. Balance verification
+     * 3. Overpayment protection
+     * 4. CEI (Checks-Effects-Interactions) pattern
+     *
+     * Process Flow:
+     * 1. Validate inputs and addresses
+     * 2. Verify repayment amount
+     * 3. Update protocol accounting
+     * 4. Verify position health
+     * 5. Execute token transfer
+     *
+     * @param tokenToPayBack The ERC20 token address being repaid
+     * @param amountToPayBack The amount of tokens to repay (in token's native decimals)
+     * @param onBehalfOf The address whose debt is being repaid (enables altruistic repayments)
+     *
+     * Security Features:
+     * - Implements reentrancy protection via modifiers
+     * - Validates token allowlist to prevent unsupported assets
+     * - Ensures accurate debt accounting
+     * - Uses internal accounting before external calls (CEI pattern)
+     */
     function _paybackBorrowedAmount(
         address tokenToPayBack,
         uint256 amountToPayBack,
@@ -77,34 +165,45 @@ contract Borrowing is Lending {
         moreThanZero(amountToPayBack)
         isAllowedToken(tokenToPayBack)
     {
-        // if the address being paid on behalf of is the 0 address, revert
+        // STAGE 1: Input Validation
+        // Prevent repayments to zero address to protect against lost funds
+        // Critical for maintaining accurate protocol accounting
         if (onBehalfOf == address(0)) {
             revert Errors.ZeroAddressNotAllowed();
         }
 
-        // Check if payer has enough tokens to pay the debt
+        // STAGE 2: Balance Verification
+        // Ensure payer has sufficient tokens to cover the repayment
+        // Prevents transaction failures and gas waste
         if (IERC20(tokenToPayBack).balanceOf(msg.sender) < amountToPayBack) {
             revert Errors.Borrowing__NotEnoughTokensToPayDebt();
         }
 
-        // Safety check to prevent users from overpaying their debt
+        // STAGE 3: Overpayment Protection
+        // Verify repayment amount doesn't exceed outstanding debt
+        // Maintains accurate debt accounting and prevents excess payments
         uint256 borrowedAmount = _getAmountOfTokenBorrowed(onBehalfOf, tokenToPayBack);
         if (borrowedAmount < amountToPayBack) {
             revert Errors.Borrowing__OverpaidDebt();
         }
 
-        // Update state BEFORE external calls (CEI pattern)
-        // Decrease the user's borrowed balance in our internal accounting
-        // This must happen first to prevent reentrancy attacks
+        // STAGE 4: State Updates (Following CEI Pattern)
+        // Update internal accounting before any external interactions
+        // Critical for preventing reentrancy attacks and maintaining accurate state
         decreaseUserDebtAndTotalDebtBorrowed(onBehalfOf, tokenToPayBack, amountToPayBack);
 
-        // emit event
+        // Emit event for off-chain tracking and transparency
+        // Essential for DApp interfaces and protocol monitoring
         emit BorrowedAmountRepaid(msg.sender, onBehalfOf, tokenToPayBack, amountToPayBack);
 
-        // Check health factor after repayment just in case
+        // STAGE 5: Health Factor Validation
+        // Verify position health after repayment
+        // Ensures position remains within protocol safety parameters
         _revertIfHealthFactorIsBroken(onBehalfOf);
 
-        // Transfer tokens from user to contract
+        // STAGE 6: Token Transfer (Final Interaction)
+        // Execute the actual token transfer from payer to protocol
+        // Last step in accordance with CEI pattern
         bool success = IERC20(tokenToPayBack).transferFrom(msg.sender, address(this), amountToPayBack);
 
         // Check if transfer was successful
@@ -114,37 +213,136 @@ contract Borrowing is Lending {
         }
     }
 
+    /*//////////////////////////////////////////////////////////////
+                         PRIVATE VIEW FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Calculates the total amount of a specific token held as collateral by the protocol
+     * @dev Implements an efficient token balance lookup with the following features:
+     *
+     * Architecture Highlights:
+     * 1. Linear search through token list (checks each token until match found)
+     * 2. Early exit optimization (stops searching after finding match)
+     * 3. Direct balance querying
+     * 4. Zero gas waste on unnecessary iterations
+     *
+     * Implementation Strategy:
+     * - Iterates through allowed tokens array
+     * - Matches target token against allowed tokens
+     * - Returns actual balance when match found
+     * - Uses break statement to optimize gas usage
+     *
+     * @param token The ERC20 token address to check total collateral for
+     * @return totalCollateral The total amount of tokens held by the contract in native decimals
+     *
+     * Gas Optimization Features:
+     * - Single loop iteration for token lookup
+     * - Early break on token match
+     * - View function for gas-free calls
+     * - Minimal storage reads using memory variables
+     */
     function _getTotalCollateralOfToken(address token) private view returns (uint256 totalCollateral) {
-        // loop through the allowed collateral tokens
+        // STAGE 1: Token List Iteration
+        // Efficiently search through allowed tokens list
+        // Uses cached array length to prevent multiple storage reads
         for (uint256 i = 0; i < _getAllowedTokens().length; i++) {
-            // if the token inputted by the caller is in the loop, then
+            // STAGE 2: Token Matching
+            // Compare current token against target token
+            // Early exit pattern for gas optimization
             if (_getAllowedTokens()[i] == token) {
-                // Get actual token balance of contract
+                // STAGE 3: Balance Retrieval
+                // Direct query of contract's token balance
+                // Returns actual physical tokens held by contract
                 totalCollateral = IERC20(token).balanceOf(address(this));
-                // exit loop immediately
+
+                // STAGE 4: Early Exit
+                // Break loop immediately after finding match
+                // Saves gas by preventing unnecessary iterations
                 break;
             }
         }
-        // return the total amount of collateral of these tokens
+
+        // STAGE 5: Return Result
+        // Return total collateral amount
+        // Will be 0 if token not found in allowed list
         return totalCollateral;
     }
 
-    function getTotalCollateralOfToken(address token) external view returns (uint256 totalCollateral) {
-        return _getTotalCollateralOfToken(token);
-    }
-
+    /**
+     * @notice Calculates the available amount of tokens that can be borrowed from the protocol
+     * @dev Implements a precise liquidity calculation system with the following features:
+     *
+     * Calculation Method:
+     * 1. Total Collateral Tracking
+     *    - Queries physical token balance
+     *    - Considers all deposited collateral
+     *    - Real-time balance updates
+     *
+     * 2. Borrowed Amount Tracking
+     *    - Monitors all outstanding loans
+     *    - Aggregates total borrowed amount
+     *    - Protocol-wide debt tracking
+     *
+     * 3. Available Liquidity
+     *    - Dynamic calculation: Total Collateral - Total Borrowed
+     *    - Ensures protocol solvency
+     *    - Prevents over-borrowing
+     *
+     * @param token The ERC20 token address to check available liquidity for
+     * @return uint256 The amount of tokens available for borrowing in native decimals
+     *
+     * Security Features:
+     * - View function for gas-free queries
+     * - Real-time balance checking
+     * - Underflow protected subtraction
+     * - Accurate protocol-wide accounting
+     */
     function _getAvailableToBorrow(address token) private view returns (uint256) {
-        // Get total amount of this token deposited as collateral
+        // STAGE 1: Total Collateral Calculation
+        // Query the total amount of this token held as collateral
+        // Represents the maximum theoretical amount available
         uint256 totalCollateral = _getTotalCollateralOfToken(token);
 
-        // Get total amount already borrowed of this token
+        // STAGE 2: Total Borrowed Calculation
+        // Get the total amount currently borrowed across all users
+        // Represents the amount already committed to borrowers
         uint256 totalBorrowed = _getTotalTokenAmountsBorrowed(token);
 
-        // Available = Total Collateral - Total Borrowed
+        // STAGE 3: Available Liquidity Calculation
+        // Calculate actual available liquidity by subtracting borrowed from total
+        // This ensures we never over-commit tokens to borrowers
         return totalCollateral - totalBorrowed;
     }
 
+    /*//////////////////////////////////////////////////////////////
+                        EXTERNAL VIEW FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice External interface for querying total protocol collateral for a specific token
+     * @dev Provides a secure public access point to internal collateral tracking with the following features:
+     *
+     * @param token The ERC20 token address to check total collateral for
+     * @return totalCollateral The total amount of tokens held by the protocol in native decimals
+     *  View function for gas-efficient queries
+     */
+    function getTotalCollateralOfToken(address token) external view returns (uint256 totalCollateral) {
+        // Calls private implementation to save gas
+        return _getTotalCollateralOfToken(token);
+    }
+
+    /**
+     * @notice External interface for querying available protocol liquidity for borrowing
+     * @dev Provides essential liquidity data for protocol users and integrations
+     *
+     * @param token The ERC20 token address to check borrowing availability for
+     * @return uint256 The amount of tokens available for borrowing in native decimals
+     *
+     * View function for gas-efficient queries
+     */
     function getAvailableToBorrow(address token) external view returns (uint256) {
+        // Calls private implementation to save gas
         return _getAvailableToBorrow(token);
     }
 }
