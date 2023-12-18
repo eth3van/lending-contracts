604c51a7f79997bcda0e1b4a639694b7b722afac
eth3van <evanguo.eth@gmail.com>
2023-12-18 09:21:35 -0500

    clean up

--- src/libraries/Errors.sol
+++ src/libraries/Errors.sol
@@ -1,63 +1,219 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+/**
+ * @title Errors Library
+ * @author Evan Guo
+ * @notice Custom error definitions for the LendingCore protocol
+ * @dev Centralized error handling using custom errors for gas efficiency and improved debugging
+ *      Each error is categorized by the component it belongs to
+ */
 library Errors {
-    ///////////////////////////////
-    //      Lending Errors      //
-    ///////////////////////////////
+    /*//////////////////////////////////////////////////////////////
+                           LENDING ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Thrown when a user attempts to perform an action without sufficient funds
+     * @dev Used in lending operations to prevent invalid token transfers
+     */
     error Lending__YouNeedMoreFunds();
 
-    ///////////////////////////////
-    //     Borrowing Errors     //
-    ///////////////////////////////
+    /*//////////////////////////////////////////////////////////////
+                           BORROWING ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Thrown when a user attempts to repay more than their outstanding debt
+     * @dev Prevents overpayment scenarios and ensures accurate debt tracking
+     */
     error Borrowing__OverpaidDebt();
+
+    /**
+     * @notice Thrown when attempting to borrow more than available protocol liquidity
+     * @dev Ensures protocol always maintains sufficient liquidity for withdrawals
+     */
     error Borrowing__NotEnoughAvailableCollateral();
+
+    /**
+     * @notice Thrown when a user attempts to repay debt without sufficient token balance
+     * @dev Validates user has enough tokens before initiating repayment
+     */
     error Borrowing__NotEnoughTokensToPayDebt();
 
-    ///////////////////////////////
-    //    CoreStorage Errors    //
-    ///////////////////////////////
+    /*//////////////////////////////////////////////////////////////
+                          CORE STORAGE ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Thrown when token and price feed arrays have mismatched lengths during initialization
+     * @dev Critical for maintaining price feed integrity for all supported tokens
+     */
     error CoreStorage__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+
+    /**
+     * @notice Thrown when requested batch size exceeds protocol limits
+     * @dev Prevents excessive gas consumption in batch operations
+     */
     error CoreStorage__BatchSizeTooLarge();
 
-    ///////////////////////////////
-    //   HealthFactor Errors    //
-    ///////////////////////////////
-    error HealthFactor__BreaksHealthFactor(uint256);
+    /*//////////////////////////////////////////////////////////////
+                          HEALTH FACTOR ERRORS
+    //////////////////////////////////////////////////////////////*/
 
-    ///////////////////////////////
-    //      Shared Errors       //
-    ///////////////////////////////
+    /**
+     * @notice Thrown when an action would break the minimum health factor requirement
+     * @dev Includes the resulting health factor for detailed error reporting
+     * @param healthFactor The calculated health factor that would result from the action
+     */
+    error HealthFactor__BreaksHealthFactor(uint256 healthFactor);
+
+    /*//////////////////////////////////////////////////////////////
+                            SHARED ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Thrown when an operation requires a non-zero amount
+     * @dev Common validation across multiple protocol operations
+     */
     error AmountNeedsMoreThanZero();
+
+    /**
+     * @notice Thrown when attempting to use an unsupported token
+     * @dev Includes token address for easier debugging
+     * @param token Address of the unsupported token
+     */
     error TokenNotAllowed(address token);
+
+    /**
+     * @notice Thrown when a token transfer fails
+     * @dev Critical for maintaining protocol safety during token operations
+     */
     error TransferFailed();
+
+    /**
+     * @notice Thrown when an operation requires a non-zero address
+     * @dev Prevents operations with invalid addresses
+     */
     error ZeroAddressNotAllowed();
 
-    ///////////////////////////////
-    //    Liquidation Errors    //
-    //////////////////////////////
+    /*//////////////////////////////////////////////////////////////
+                          LIQUIDATION ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Thrown when attempting to liquidate a healthy position
+     * @dev Prevents unnecessary liquidations of safe positions
+     */
     error Liquidations__HealthFactorIsHealthy();
+
+    /**
+     * @notice Thrown when a liquidation doesn't improve the position's health factor
+     * @dev Ensures liquidations are beneficial to protocol health
+     */
     error Liquidations__HealthFactorNotImproved();
+
+    /**
+     * @notice Thrown when liquidator has insufficient balance to repay debt
+     * @dev Validates liquidator resources before initiating liquidation
+     */
     error Liquidations__InsufficientBalanceToLiquidate();
+
+    /**
+     * @notice Thrown when user attempts to liquidate their own position
+     * @dev Prevents potential manipulation through self-liquidation
+     */
     error Liquidations__CantLiquidateSelf();
+
+    /**
+     * @notice Thrown when attempting to repay more debt than borrowed
+     * @dev Maintains accurate debt accounting during liquidations
+     */
     error Liquidations__DebtAmountPaidExceedsBorrowedAmount();
+
+    /**
+     * @notice Thrown when attempting to liquidate non-existent debt
+     * @dev Validates liquidation targets have outstanding debt
+     */
     error Liquidation__UserHasNotBorrowedToken();
+
+    /**
+     * @notice Thrown when regular liquidation attempted with insufficient bonus
+     * @dev Ensures protocol safety during market stress
+     */
     error Liquidations__OnlyProtocolCanLiquidateInsufficientBonus();
+
+    /**
+     * @notice Thrown when protocol liquidation fails
+     * @dev Critical error requiring immediate attention
+     */
     error Liquidations__ProtocolLiquidationFailed();
+
+    /**
+     * @notice Thrown when unauthorized access to protocol liquidation functions
+     * @dev Access control for automated liquidation system
+     */
     error Liquidations__OnlyProtocolOwnerOrAutomation();
+
+    /**
+     * @notice Thrown when setting invalid automation contract address
+     * @dev Validates automation system configuration
+     */
     error Liquidations__InvalidAutomationContract();
+
+    /**
+     * @notice Thrown when protocol fee calculation fails
+     * @dev Ensures accurate fee handling during liquidations
+     */
     error Liquidations__ProtocolFeeCalculationError();
+
+    /**
+     * @notice Thrown when collateral price feed returns invalid data
+     * @dev Prevents liquidations with unreliable price data
+     */
     error Liquidations__InvalidCollateralPrice();
+
+    /**
+     * @notice Thrown when no positions are available for liquidation
+     * @dev Used in batch liquidation processing
+     */
     error Liquidations__NoPositionsToLiquidate();
 
-    //////////////////////////////
-    //     Withdraw Errors     //
-    /////////////////////////////
+    /*//////////////////////////////////////////////////////////////
+                           WITHDRAW ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Thrown when user attempts to withdraw more than their deposit
+     * @dev Prevents unauthorized collateral withdrawal
+     */
     error Withdraw__UserDoesNotHaveThatManyTokens();
+
+    /**
+     * @notice Thrown when user attempts to withdraw without collateral
+     * @dev Validates user has deposited before withdrawal
+     */
     error Withdraw__UserHasNoCollateralDeposited();
 
-    //////////////////////////////
-    //      LendingCore Errors      //
-    /////////////////////////////
+    /*//////////////////////////////////////////////////////////////
+                            LENDING CORE ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Thrown when unauthorized access to LiquidationEngine functions
+     * @dev Core protocol access control
+     */
     error LendingCore__OnlyLiquidationEngine();
+
+    /**
+     * @notice Thrown when handling direct ETH transfers to the contract
+     * @dev Prevents accidental ETH locks and forces use of wrapped ETH (WETH)
+     */
+    error LendingCore__NoDirectETHTransfers();
+
+    /**
+     * @notice Thrown when handling fallback calls to undefined functions
+     * @dev Prevents unknown function calls and protects against erroneous calls
+     */
+    error LendingCore__FunctionNotFound();
 }
